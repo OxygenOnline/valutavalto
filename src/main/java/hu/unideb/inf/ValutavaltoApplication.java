a70a5496f4fedd59e824dc9bb0823e0922ab7722
Farkas Fruzsina Diána <fruzsinadiana@mailbox.unideb.hu>
2023-10-24 14:08:12 +0200

    Initial commit

---
+++ src/main/java/hu/unideb/inf/ValutavaltoApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValutavaltoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValutavaltoApplication.class, args);
+	}
+
+}
