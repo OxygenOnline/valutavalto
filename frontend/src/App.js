6df879d69894f1acad83fbcc0045440f6cc48ba7
Dimand Márton <dimand.marton@gmail.com>
2023-11-11 15:43:11 +0100

    Connected frontend to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,120 @@
-import React from 'react';
-import { Row, Col, Form, Container} from 'react-bootstrap';
+import React, {useEffect, useState} from 'react';
+import {Row, Col, Form, Container, Button} from 'react-bootstrap';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import InputAmount from './InputAmount';
 import ConvertedAmount from './ConvertedAmount';
 
 import currencies from './currency_list';
+import axios from "axios";
 function App() {
+
+  const [fromCurrencyAmount, setFromCurrencyAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("EUR");
+
+  const [toCurrencyAmount, setToCurrencyAmount] = useState(0);
+  const [toCurrency, setToCurrency] = useState("HUF");
+
+
+  useEffect(() => {
+    axios.defaults.baseURL = "http://localhost:9090";
+    console.log(`Conversion from: ${fromCurrency} to: ${toCurrency} with amount: ${fromCurrencyAmount}`)
+    axios.get(`/exchangerates/latest/convert?from=${fromCurrency}&to=${toCurrency}&amount=${fromCurrencyAmount}`) //TODO
+        .then(response => {
+          setToCurrencyAmount(response.data); // Assuming the response contains the number value
+        })
+        .catch(error => console.error('Error fetching data: ', error));
+  }, [toCurrency, fromCurrencyAmount, fromCurrency]);
+
+  useEffect(() => {
+    // Fetch data from the backend
+
+  }, []);
+
+
+
+  function handleFromCurrencyAmountChange(event) {
+    event.preventDefault();
+    setFromCurrencyAmount(event.target.value);
+  }
+  function handleFromCurrencyChange(event) {
+    event.preventDefault();
+    setFromCurrency(event.target.value);
+  }
+
+  function handleToCurrencyAmountChange(event) {
+    event.preventDefault();
+    setToCurrencyAmount(event.target.value);
+  }
+
+  function handleToCurrencyChange(event) {
+    event.preventDefault();
+    setToCurrency(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Container className='mb-4'>
-          <h1>Currency Converter</h1>
-        </Container>
-        <Container>
-          <Row>
-            <Col md>
-              <InputAmount/>
-            </Col>
-            <Col md>
-              <Form.Select>
-                <option key='blankChoice' hidden value>
-                  Target Currency
-                </option>
-                {currencies.map((currency) => (
-                    <option key={currency}>{currency}</option>
-                ))}
-              </Form.Select>
-            </Col>
-            <Col md>
-              <ConvertedAmount/>
-            </Col>
-          </Row>
-        </Container>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <Container className='mb-4'>
+            <h1>Currency Converter</h1>
+          </Container>
+          <Container>
+            <Row>
+              <Col md>
+                <Row>
+                  <Form>
+                    <Col>
+                      <Form.Group>
+                        <Form.Control
+                            name="FromCurrencyAmount"
+                            type="number"
+                            placeholder="Enter the amount"
+                            value={fromCurrencyAmount}
+                            onChange={handleFromCurrencyAmountChange}
+                        />
+                      </Form.Group>
+                    </Col>
+                    <Col>
+                      <Form.Select name="FromCurrencyList" onChange={handleFromCurrencyChange} value={fromCurrency}>
+                        {currencies.map((currency) => (
+                            <option key={currency}>{currency}</option>
+                        ))}
+                      </Form.Select>
+                    </Col>
+                  </Form>
+                </Row>
+              </Col>
+              <Col>
+                Equals
+              </Col>
+              <Col md>
+                <Row>
+                  <Form>
+                    <Col>
+                      <Form.Group>
+                        <Form.Control
+                            disabled={true}
+                            name="ToCurrencyAmount"
+                            type="number"
+                            value={toCurrencyAmount}
+                            onChange={handleToCurrencyAmountChange}
+                        />
+                      </Form.Group>
+                    </Col>
+                    <Col>
+                      <Form.Select name="ToCurrencyList" onChange={handleToCurrencyChange} value={toCurrency}>
+                        {currencies.map((currency) => (
+                            <option key={currency}>{currency}</option>
+                        ))}
+                      </Form.Select>
+                    </Col>
+                  </Form>
+                </Row>
+              </Col>
+            </Row>
+          </Container>
+        </header>
+      </div>
   );
 }
 
